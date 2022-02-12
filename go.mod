c11a00d325976f610b989c76e3d4912994480015
root <lsq51201314@126.com>
2022-02-12 14:53:17 +0800

    完美世界EL116(v17)读取

---
+++ go.mod
@@ -0,0 +1,9 @@
+module pw_el116
+
+go 1.16
+
+require (
+	golang.org/x/text v0.3.6
+	gorm.io/driver/mysql v1.1.1
+	gorm.io/gorm v1.21.12
+)
---
+++ utils/byteEx/byteEx.go
@@ -0,0 +1,56 @@
+package byteEx
+
+import (
+	"bytes"
+	"encoding/binary"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
+	"io/ioutil"
+	"math"
+	"os"
+)
+
+func GetFileByte(filepath string, offset, length int64) (data []byte, err error) {
+	var file *os.File
+	if file, err = os.Open(filepath); err == nil {
+		buf := make([]byte, length)
+		var n int
+		if n, err = file.ReadAt(buf, offset); err == nil {
+			data = buf[:n]
+		}
+		file.Close()
+	}
+	return
+}
+
+func BytesToIntLittle(b []byte) int {
+	bytesBuffer := bytes.NewBuffer(b)
+	var x int32
+	binary.Read(bytesBuffer, binary.LittleEndian, &x)
+	return int(x)
+}
+
+func ByteToFloatLittle(bytes []byte) float32 {
+	bits := binary.LittleEndian.Uint32(bytes)
+	return math.Float32frombits(bits)
+}
+
+func WStrByteToStr(wsbyte []byte) string {
+	c := unicode.UTF16(unicode.LittleEndian, unicode.IgnoreBOM).NewDecoder()
+	d, ok := c.Bytes(wsbyte)
+	if ok != nil {
+		return ""
+	}
+	d = bytes.TrimRight(d, "\x00")
+	return string(d)
+}
+
+func ByteToStr(sbyte []byte) string {
+	d, ok := ioutil.ReadAll(transform.NewReader(bytes.NewReader(sbyte), simplifiedchinese.GBK.NewDecoder()))
+	if ok != nil {
+		return ""
+	}
+	d = bytes.TrimRight(d, "\x00")
+	return string(d)
+}
